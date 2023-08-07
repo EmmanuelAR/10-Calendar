a577e801af8e70d6f0dd853b5e3b160c7ae32a43
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2023-08-07 11:01:46 -0600

    test in approuter

---
+++ tests/router/AppRouter.test.jsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import { useAuthStore } from "../../src/hooks";
+import { AppRouter } from "../../src/router/AppRouter";
+
+jest.mock("../../src/hooks/useAuthStore");
+
+describe("Pruebas en el AppRouter", () => {
+  const mockcheckAuthToken = jest.fn();
+  beforeEach(() => jest.clearAllMocks());
+
+  test("Debe de mostrar la pantalla de carga y llamar al checkAuthToken", () => {
+    useAuthStore.mockReturnValue({
+      status: "checking",
+      checkAuthToken: mockcheckAuthToken,
+    });
+    render(<AppRouter />);
+    expect(mockcheckAuthToken).toHaveBeenCalled();
+    expect(screen.getByText("Cargando...")).toBeTruthy();
+  });
+
+  test("Debe de mostrar el login en caso de no estar autenticado", () => {
+    useAuthStore.mockReturnValue({
+      status: "checking",
+      checkAuthToken: mockcheckAuthToken,
+    });
+    render(<AppRouter />);
+    screen.debug();
+    expect(mockcheckAuthToken).toHaveBeenCalled();
+    expect(screen.getByText("Cargando...")).toBeTruthy();
+  });
+});
